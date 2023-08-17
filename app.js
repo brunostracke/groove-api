3600a995d6ceb160b48b1eb494e65ee6abda3746
brunostracke <brunostracke@gmail.com>
2023-08-17 19:42:19 -0300

    change response from obj to array

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 const token = process.env.SYMPLA_TOKEN;
 const baseApiUrl = process.env.SYMPLA_API;
@@ -14,36 +14,46 @@ app.use(cors());
 app.get("/", async (req, res) => {
   const { data } = await axios.get(`${baseApiUrl}/events/2113137/orders`, {
     headers: {
-      "s_token": token
-    }
-  })
-  const ids = data.data.map(dt => dt.id)
+      s_token: token,
+    },
+  });
+  const ids = data.data.map((dt) => dt.id);
   let tickets;
   try {
-    tickets = await Promise.all(ids.map(id => axios.get(`${baseApiUrl}/events/2113137/orders/${id}/participants`, {
-      headers: {
-        "s_token": token
-      }
-    })))
-
+    tickets = await Promise.all(
+      ids.map((id) =>
+        axios.get(`${baseApiUrl}/events/2113137/orders/${id}/participants`, {
+          headers: {
+            s_token: token,
+          },
+        })
+      )
+    );
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 
   const codes = tickets.reduce((acc, order) => {
-    const {data} = order;
-    data.data.forEach(dt => {
+    const { data } = order;
+    data.data.forEach((dt) => {
       const { order_discount } = dt;
-      const code = order_discount.split(" - ")[1]
-      if(!acc[code]) acc[code] = 0
-      acc[code]++
+      const code = order_discount.split(" - ")[1];
+      if (!acc[code]) acc[code] = 0;
+      acc[code]++;
+    });
+    return acc;
+  }, {});
+    const result = Object.keys(codes).map(code => {
+      return {
+        id: code,
+        code: code,
+        quantity: codes[code]
+      }
     })
-    return acc
-  },{})
-  res.json(codes)
-})
+  res.json(result);
+});
 
 app.listen(port, () => {
-  console.log("listening on port 3010...")
-})
+  console.log("listening on port 3010...");
+});
